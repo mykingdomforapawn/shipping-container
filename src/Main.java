bdfb1f8ae2893a3a2824daa71cfbe47427739f17
Ludwig <ludwig.nachname.spam@gmail.com>
2023-01-11 07:15:12 +0100

    Added properties file for logger

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.List;
+import java.util.logging.LogManager;
 
 public class Main {
     public static void main(String[] args) {
+        try {
+            LogManager.getLogManager().readConfiguration(new FileInputStream("logging.properties"));
+        } catch (SecurityException | IOException e1) {
+            e1.printStackTrace();
+        }
+
         System.out.println("Hello world!");
         ContainerType xx = ContainerType.TANK_CONTAINER;
         System.out.println(xx);
