b2814ebbf6abe4950aa65b888c55cb9f41124397
Ludwig <ludwig.nachname.spam@gmail.com>
2023-01-15 12:05:38 +0100

    Implemented file handler

--- src/Container.java
+++ src/Container.java
@@ -1,15 +1,25 @@
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.UUID;
-import java.util.logging.ConsoleHandler;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.logging.*;
 
 public class Container {
     private final ContainerType containerType;
     private final UUID id;
     private final int cargoWeight;
-    //private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+    private static final Logger LOGGER = Logger.getLogger(Container.class.getName());
 
     static {
+        try {
+            FileHandler fileHandler = new FileHandler("logs/" + Container.class.getName() + ".log");
+            //fileHandler.setFormatter(new SimpleFormatter());
+            fileHandler.setLevel(Level.INFO);
+            LOGGER.addHandler(fileHandler);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
         //ConsoleHandler consoleHandler = new ConsoleHandler();
         //LOGGER.addHandler(consoleHandler);
 
@@ -18,6 +28,7 @@ public class Container {
         this.containerType = containerType;
         this.cargoWeight = cargoWeight;
         this.id = UUID.randomUUID();
+        LOGGER.log(Level.INFO, "New container " + this.id + " initiated.");
         //LOGGER.logp(Level.INFO, Container.class.getName(), "Constructor", "New container with id " + this.id + " initiated");
     }
 
