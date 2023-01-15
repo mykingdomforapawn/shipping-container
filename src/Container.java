9b37680d8aff4bdb64e4878d2fdb597f1a2f27e4
Ludwig <ludwig.nachname.spam@gmail.com>
2023-01-15 12:08:57 +0100

    Changed console handler log leven

--- src/Container.java
+++ src/Container.java
@@ -1,4 +1,3 @@
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.UUID;
 import java.util.logging.*;
@@ -12,24 +11,19 @@ public class Container {
     static {
         try {
             FileHandler fileHandler = new FileHandler("logs/" + Container.class.getName() + ".log");
-            //fileHandler.setFormatter(new SimpleFormatter());
             fileHandler.setLevel(Level.INFO);
             LOGGER.addHandler(fileHandler);
         } catch (IOException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
         }
-
-        //ConsoleHandler consoleHandler = new ConsoleHandler();
-        //LOGGER.addHandler(consoleHandler);
-
     }
+
     public Container(ContainerType containerType, int cargoWeight) {
         this.containerType = containerType;
         this.cargoWeight = cargoWeight;
         this.id = UUID.randomUUID();
         LOGGER.log(Level.INFO, "New container " + this.id + " initiated.");
-        //LOGGER.logp(Level.INFO, Container.class.getName(), "Constructor", "New container with id " + this.id + " initiated");
     }
 
     public UUID getId() {
