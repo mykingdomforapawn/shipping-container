37163249a362a3cd5b74eeffa384cb70ba642377
Ludwig <ludwig.nachname.spam@gmail.com>
2023-01-15 12:14:14 +0100

    Implemented lohhing test scenarios

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,24 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.List;
 import java.util.logging.LogManager;
-import java.util.logging.Logger;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-
+        FileInputStream fis =  new FileInputStream("logging.properties");
         //LoggingUtil.initLogManager();
         try {
-            FileInputStream fis =  new FileInputStream("logging.properties");
+            //FileInputStream fis =  new FileInputStream("logging.properties");
             LogManager.getLogManager().readConfiguration(fis);
         } catch(IOException e) {
             e.printStackTrace();
         }
 
-        System.out.println("Hello world!");
-
-        // Init ship and add container
+        // Init ship and look at containers
         Ship myShip = new Ship(2, 20000, List.of(ContainerType.REEFER_CONTAINER, ContainerType.TANK_CONTAINER));
+        System.out.println("Current containes on ship: " + myShip.getContainerIDs());
+
+        // Add container
         Container myContainer = new Container(ContainerType.TANK_CONTAINER, 300);
         myShip.addContainer(myContainer);
 
@@ -36,5 +36,7 @@ public class Main {
         myShip.addContainer(new Container(ContainerType.REEFER_CONTAINER, 20));
         myShip.addContainer(new Container(ContainerType.REEFER_CONTAINER, 20));
 
+        // Look at containers
+        System.out.println("Current containes on ship: " + myShip.getContainerIDs());
     }
 }
